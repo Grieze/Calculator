548fc4e90d898f3a2e6266551e7e68764ca12a86
Grieze <61562774+Grieze@users.noreply.github.com>
2021-08-23 09:40:33 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -41,44 +41,54 @@ function operate(operation, a, b) {
 
 let display = document.getElementById('display')
 let buttons = document.querySelectorAll('button');
-let memoryVal = document.getElementById('memoryVal');
+let memoryVal1 = document.getElementById('memoryVal1');
+let memoryVal2 = document.getElementById('memoryVal2');
 let memoryOp = document.getElementById('memoryOp')
+
+function clearDisplay() {
+  display.innerHTML = "";
+}
+
+function clearAll() {
+  display.innerHTML = "";
+  memoryVal1.innerHTML = "";
+  memoryVal2.innerHTML = "";
+  memoryOp.innerHTML = "";
+}
 
 function parser(e) {
   if (e.target.innerHTML == "C") {
     clearDisplay() // clear the screen
   }
-  else if (!isNaN(e.target.innerHTML) || e.target.innerHTML == ".") {
-    if (display.innerHTML != "" && memoryVal.innerHTML == "") {
-      clearDisplay();
-      display.innerHTML = display.innerHTML + e.target.innerHTML;
-      memoryVal.innerHTML = display.innerHTML;
-    }
-    else {
-      display.innerHTML = display.innerHTML + e.target.innerHTML;
-      memoryVal.innerHTML = display.innerHTML;
-    }
+  else if (e.target.innerHTML == "AC") {
+    clearAll();
   }
+  // for digits and period
+  else if (!isNaN(e.target.innerHTML) || e.target.innerHTML == ".") {
+    display.innerHTML = display.innerHTML + e.target.innerHTML;
+  }
+  // for operations
   else if (e.target.innerHTML == "+" || e.target.innerHTML == "-" ||
            e.target.innerHTML == "*" || e.target.innerHTML == "/") {
-             if (memoryOp.innerHTML == "") {
+             if (memoryOp.innerHTML == "" && memoryVal1.innerHTML == "") {
                memoryOp.innerHTML = e.target.innerHTML;
-               memoryVal.innerHTML = display.innerHTML;
+               memoryVal1.innerHTML = display.innerHTML;
                clearDisplay();
              }
              else {
-               memoryVal.innerHTML = display.innerHTML; // copy result
+               memoryVal2.innerHTML = display.innerHTML;
                memoryOp.innerHTML = e.target.innerHTML;
              }
   }
   else {
-    display.innerHTML = operate(memoryOp.innerHTML, memoryVal.innerHTML, display.innerHTML);
+    memoryVal2.innerHTML = display.innerHTML;
+    display.innerHTML = operate(memoryOp.innerHTML, memoryVal1.innerHTML, memoryVal2.innerHTML);
+    memoryVal1.innerHTML = "";
+    memoryVal2.innerHTML = "";
+    memoryOp.innerHTML = "";
   }
 }
 
-function clearDisplay() {
-  display.innerHTML = "";
-}
 
 // we use the .forEach method to iterate through each button
 buttons.forEach((button) => {
